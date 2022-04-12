fbc5f0ead931ea688ccb216e2ce36e5180d90d9d
SiddhiNaveen1 <99012361+SiddhiNaveen1@users.noreply.github.com>
2022-04-12 10:01:21 +0530

    Add files via upload

---
+++ OLX-Login/src/main/java/com/olx/login/repository/LoginRepo.java
@@ -0,0 +1,9 @@
+package com.olx.login.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.olx.login.entity.UserEntity;
+
+public interface LoginRepo extends JpaRepository<UserEntity, Integer>{
+
+}
